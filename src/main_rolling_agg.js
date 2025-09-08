c93d82eba67268ae7a29bbbcacc435e2dc7cda0b
OliverBudras <167419824+OliverBudras@users.noreply.github.com>
2025-09-08 09:11:58 +0200

    Update main_rolling_agg.js

--- src/main_rolling_agg.js
+++ src/main_rolling_agg.js
@@ -1,6 +1,6 @@
 import Graph from "./node_modules/graphology/dist/graphology.umd.min.js";
 import Sigma from "./node_modules/sigma/dist/sigma.min.js";
-import { NodeBorderProgram } from "@sigma/node-border";
+import { NodeBorderProgram } from "./node_modules/@sigma/node-border/dist/sigma-node-border.cjs.js";
 
 const container = document.getElementById("sigma-container");
 const infoBox = document.getElementById("node-info");
